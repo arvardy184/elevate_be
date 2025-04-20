a38d4cdd0d6a17468c6fe80a691f96c864ae8dec
Arvan Ardana <113536365+arvardy184@users.noreply.github.com>
2025-04-20 19:10:49 +0700

    tes

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -4,6 +4,11 @@ const express = require('express');
 const router = express.Router();
 const AuthController = require('../controllers/auth_controller');
 
+
+//tes route
+router.get('/', (req, res) => {
+    res.send('Selamat datang di API dengan Node.js, Express.js, MySQL, dan JWT!');
+})
 // REGISTER
 router.post('/register', AuthController.register);
 
