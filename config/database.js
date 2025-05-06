524385a4f9add89100c68d8c09717c9f68bb79c3
Arvan Ardana <113536365+arvardy184@users.noreply.github.com>
2025-05-06 18:27:57 +0700

    fix: env & db.js

--- config/database.js
+++ config/database.js
@@ -2,15 +2,20 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  host: process.env.DB_HOST || '127.0.0.1',  // Menggunakan localhost jika variabel tidak ada
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '12345678',
+  database: process.env.DB_NAME || 'elevate'
 });
+console.log('DB_HOST:', process.env.DB_HOST);   // Pastikan variabel terbaca dengan benar
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_NAME:', process.env.DB_NAME);
+
 
 db.connect((err) => {
   if (err) {
-    console.error('Koneksi DB error: ', err);
+    console.error('Koneksi DB error: ', err.message);  // Menampilkan pesan error yang lebih jelas
     return;
   }
   console.log('Terhubung ke database MySQL!');
