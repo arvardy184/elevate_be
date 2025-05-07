81161b3bdda33ae45659a015cb10286101a2b17c
Arvan Ardana <113536365+arvardy184@users.noreply.github.com>
2025-05-07 13:11:33 +0700

    fix: mailer

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,33 +1,30 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    service: "Gmail",
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true, // true for 465, false for other ports
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
+  service: "Gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // true for 465, false for other ports
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
 });
 
 exports.sendOtpEmail = async (to, otp) => {
-    const mailOptions = {
-        from: process.env.EMAIL_USER,
-        to: to,
-        subject: 'OTP Verification',
-        text: `Your OTP is: ${otp}`,
-    };
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: to,
+    subject: "OTP Verification",
+    text: `Your OTP is: ${otp}`,
+  };
 
-    try {
-        // Send the email and wait for the result
-        const info = await transporter.sendMail(mailOptions);
-        console.log('Email sent: ' + info.response);
-    } catch (error) {
-        // Log the error if email sending fails
-        console.log('Error sending email:', error);
-    }
-
-
-    await transporter.sendMail(mailOptions);
-}
+  try {
+    // Send the email and wait for the result
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email sent: " + info.response);
+  } catch (error) {
+    // Log the error if email sending fails
+    console.log("Error sending email:", error);
+  }
+};
